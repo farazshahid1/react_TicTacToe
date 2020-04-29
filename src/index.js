d23f4da8c45250bb2629218c4fbac913a9bc2e15
farazshahid1 <farazshahid123456@gmail.com>
2020-04-29 23:55:43 +0500

    show past moves done

--- src/index.js
+++ src/index.js
@@ -81,12 +81,24 @@ class Game extends Component {
 
     const history = this.state.history;
     const current = history[history.length-1];
-
     const winner = calculateWinner(current.squares)
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 
+        'Go to move #' + move :
+        'Go to game Start';
+
+        return (
+          <li>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+    });
+
     let status;
     if(winner){
       status = 'Winner: ' + winner;
-    }else {
+    } else {
       status = 'Next Player: ' + this.state.xIsNext ? 'X' : 'O';
     }
 
@@ -99,7 +111,7 @@ class Game extends Component {
        </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     )
